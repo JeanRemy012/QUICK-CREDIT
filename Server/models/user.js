4ccd12299f2f25ed936175c609c29b342e8bb4f9
Jean Remy HARERIMANA <remyson012@gmail.com>
2019-05-18 11:07:11 +0200

    Feature Create login and Signup end points

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,7 +1,18 @@
 import users from '../data/user.json';
+import fs from 'fs';
+import path from 'path';
 
 const getUsers = () => {
     return users;
 }
 
+export const saveUser = (user) => {
+    const savePath = path.resolve(__dirname, '../data/user.json');
+    console.log(savePath);
+    const newUsers = [...users, user];
+    fs.writeFileSync(savePath, JSON.stringify(newUsers), {
+        encoding: ''
+    });
+    return user;
+}
 export default getUsers
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import authenticateUser from '../controllers/login';
-
+import signup from '../controllers/signup';
 const router = express.Router();
 
 router.post('/login', authenticateUser);
+router.post('/signup', signup);
 // router.post('/new-account', registerUser);
 export default router;
